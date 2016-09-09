a50d8e215f141694afb604f49499552b7cb70ee2
Olivier Tharan <olivier@tharan.org>
2016-09-09 12:19:45 +0200

    Add a simple initial setup script
    This will copy files to their location and touch a file. Remove
    ~/.initial-setup-done and rerun the script to start again, but this
    will override local changes.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+# Make sure some directories exist
+for d in ~/.zsh.d ~/.emacs.d/lisp ~/.zfunctions ~/.i3 ; do
+    mkdir -p $d
+done
+
+# TODO(olive): distinguish between Linux and MacOS
+# Copy files
+ISDF=~/.initial-setup-done
+if [ ! -f $ISDF ]; then
+    for f in .emacs.d/* .gitconfig .i3/config .i3status.conf .zfunctions/* .zshrc ; do
+	cp $f ~/$(dirname $f)
+    done
+    touch $ISDF
+fi
