04d1803d2d3571d8238b3b069764f2ccb8a4bd5e
Olivier Tharan <olivier@tharan.org>
2016-11-17 22:03:51 +0100

    Improve formatting

--- setup.sh
+++ setup.sh
@@ -6,10 +6,24 @@ for d in ~/.zsh.d ~/.emacs.d/lisp ~/.zfunctions ~/.i3 ~/.config/dunst ~/.config/
 done
 
 # TODO(olive): distinguish between Linux and MacOS
+# TODO(olive): probably use $(hostname) as a git branch name and make sure we're in it before copying files. Maybe do this automatically back-n-forth? Maybe use UUID instead of hostname to make it more consistent?
 # Copy files
 ISDF=~/.initial-setup-done
 if [ ! -f $ISDF ]; then
-    for f in .emacs.d/* .gnus.el .config/dunst/dunstrc .config/terminator/config .gitconfig .i3/config .i3/autostart .i3status.conf .irssi/config .zfunctions/* .zshrc ; do
+    for f in \
+        .emacs.d/* \
+            .gnus.el \
+            .config/dunst/dunstrc \
+            .config/terminator/config \
+            .gitconfig \
+            .i3/config \
+            .i3/autostart \
+            .i3status.conf \
+            .irssi/config \
+            .tmux.conf \
+            .zfunctions/* \
+            .zshrc \
+        ; do
 	cp $f ~/$(dirname $f)
     done
     touch $ISDF
